510fcf8bc8f9b9cfb2eb70a66e35035682761ac0
NCrashed <NCrashed@gmail.com>
2014-10-09 23:51:14 +0400

    Basic server for hosting lua files

--- source/app.d
+++ source/app.d
@@ -1,6 +1,20 @@
 import std.stdio;
+import vibe.d;
 
-void main()
+void index(HTTPServerRequest req, HTTPServerResponse res)
 {
-	writeln("Edit source/app.d to start your project.");
+	res.render!("index.dt", req);
+}
+
+shared static this()
+{
+	auto router = new URLRouter;
+	router.get("/", &index);
+	router.get("*", serveStaticFiles("./public/"));
+	
+	auto settings = new HTTPServerSettings;
+	settings.bindAddresses = ["127.0.0.1"];
+	settings.port = 8080;	
+	
+	listenHTTP(settings, router);
 }
